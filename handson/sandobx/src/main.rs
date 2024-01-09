cab3a3347b4d05069f8874b45bc63af5630695bc
kokoichi206 <tt.300607@gmail.com>
2024-01-09 22:46:27 +0900

    sec 3

--- handson/sandobx/src/main.rs
+++ handson/sandobx/src/main.rs
@@ -14,7 +14,56 @@ fn input(prompt: &str) -> f64 {
     return input.trim().parse().expect("数値変換エラー");
 }
 
+fn sum_slice(items: &[i32]) -> i32 {
+    let mut sum = 0;
+    for i in items {
+        sum += i;
+    }
+    return sum;
+}
+
+// tuple を構造体として定義する。
+// 文字列は &str 型ではなく String 型を使う。
+struct Person(String, u32);
+
+// シンプルな構造体。
+// 上のタプルのレイトどう違う？？
+struct CarSpec {
+    maker: String,
+    model: String,
+    year: u32,
+    color: String,
+}
+
 fn main() {
+    // tuple
+    let p = Person("Taro".to_string(), 20);
+    println!("name: {}, age: {}", p.0, p.1);
+
+    let s = String::from("Hello Rust");
+    // slice を作成する。
+    // s1 は &str 型。
+    let s1 = &s[0..5];
+    println!("s1: {}", s1);
+
+    let car = CarSpec {
+        maker: "Toyota".to_string(),
+        model: "Prius".to_string(),
+        year: 2015,
+        color: "blue".to_string(),
+    };
+    println!("car: {} {} ({}) {}", car.maker, car.model, car.year, car.color);
+
+    // from array
+    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let sum = sum_slice(&a[..]);
+    println!("sum: {}", sum);
+
+    // from vector
+    let b = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let sum = sum_slice(&b[..]);
+    println!("sum: {}", sum);
+
     // コマンドライン引数を得る。
     let args = std::env::args();
     let mut total = 0.0;
