0a4660615002f0efdd882a429e1d14de90608a23
kokoichi206 <tt.300607@gmail.com>
2024-01-15 23:00:12 +0900

    validate request body

--- web-todo/src/handlers.rs
+++ web-todo/src/handlers.rs
@@ -1,15 +1,50 @@
 use axum::{
-    extract::{Path, State},
+    extract::{rejection::FormRejection, Form, FromRequest, Path, Request, State},
     http::StatusCode,
     response::IntoResponse,
-    Json,
+    BoxError, Json,
 };
+use serde::de::DeserializeOwned;
+use std::sync::Arc;
+use validator::Validate;
+
+use async_trait::async_trait;
 
 use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
 
+#[derive(Debug)]
+pub struct ValidatedJson<T>(T);
+
+// trait 内での async を可能にするためのマクロ。
+#[async_trait]
+impl<T, S> FromRequest<S> for ValidatedJson<T>
+where
+    T: DeserializeOwned + Validate,
+    S: Send + Sync,
+    Form<T>: FromRequest<S, Rejection = FormRejection>,
+{
+    type Rejection = (StatusCode, String);
+
+    // see: https://github.com/tokio-rs/axum/blob/axum-v0.7.4/examples/validator/src/main.rs
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        let Json(value) = Json::<T>::from_request(req, state)
+            .await
+            .map_err(|rejection| {
+                let message = format!("Json parse error: {}", rejection);
+                (StatusCode::BAD_REQUEST, message)
+            })?;
+
+        value.validate().map_err(|rejection| {
+            let message = format!("Validation error: {}", rejection);
+            (StatusCode::BAD_REQUEST, message)
+        })?;
+        Ok(ValidatedJson(value))
+    }
+}
+
 pub async fn create_todo<T: TodoRepository + 'static>(
     State(repository): State<T>,
-    Json(payload): Json<CreateTodo>,
+    ValidatedJson(payload): ValidatedJson<CreateTodo>,
 ) -> impl IntoResponse {
     let todo = repository.create(payload);
 
@@ -32,7 +67,7 @@ pub async fn all_todo<T: TodoRepository>(State(repository): State<T>) -> impl In
 pub async fn update_todo<T: TodoRepository>(
     State(repository): State<T>,
     Path(id): Path<i32>,
-    Json(payload): Json<UpdateTodo>,
+    ValidatedJson(payload): ValidatedJson<UpdateTodo>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let todo = repository
         .update(id, payload)
--- web-todo/src/repositories.rs
+++ web-todo/src/repositories.rs
@@ -6,6 +6,7 @@ use std::{
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use validator::Validate;
 
 // repository で発送しうるエラーの定義。
 #[derive(Debug, Error)]
@@ -30,13 +31,17 @@ pub struct Todo {
     completed: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+    #[validate(length(min = 1, message = "Can not be empty."))]
+    #[validate(length(max = 100, message = "Text should be less than 100 characters."))]
     text: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+    #[validate(length(min = 1, message = "Can not be empty."))]
+    #[validate(length(max = 100, message = "Text should be less than 100 characters."))]
     text: Option<String>,
     completed: Option<bool>,
 }
