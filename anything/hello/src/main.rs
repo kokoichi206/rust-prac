24adbe1cde0bf2d175432c5cb4df21af544224c3
kokoichi206 <tt.300607@gmail.com>
2024-08-06 00:06:40 +0900

    Rust sec 4

--- anything/hello/src/main.rs
+++ anything/hello/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, rc::Rc};
+
 use hello::{code_block, header, ulist};
 
 fn main() {
@@ -9,4 +11,48 @@ fn main() {
     .join("\n\n");
 
     println!("{}", content);
+
+    struct Person {
+        name: Option<String>,
+        birth: i32,
+    }
+    let mut composers = Vec::new();
+    composers.push(Person {
+        name: Some("Pien".to_string()),
+        birth: 1525,
+    });
+    let first_name = composers[0].name.take();
+    print!("First name: {:?}", first_name);
+    println!("Birth year: {:?}", composers[0].birth);
+
+    let mut v = Vec::new();
+    for i in 101..106 {
+        v.push(i.to_string());
+    }
+
+    let second = v.swap_remove(2);
+    println!("Second element: {}", second);
+    println!("Vector: {:?}", v);
+
+    let s: Rc<String> = Rc::new("Xxxx".to_string());
+    let t: Rc<String> = s.clone();
+    let u: Rc<String> = s.clone();
+    // s.push_str("pien");
+    println!("s: {}, t: {}, u: {}", s, t, u);
+
+    // 借用
+    type Table = HashMap<String, Vec<String>>;
+    fn show(table: &Table) {
+        for (artist, works) in table {
+            println!("works by {}: ", artist);
+            for work in works {
+                println!("  {}", work);
+            }
+        }
+    }
+    let mut table = Table::new();
+    table.insert("Pi".to_string(), vec!["afa".to_string(), "fea".to_string()]);
+    table.insert("Piyo".to_string(), vec!["afa".to_string(), "fea".to_string()]);
+    show(&table);
+    println!("table: {:?}", table);
 }
